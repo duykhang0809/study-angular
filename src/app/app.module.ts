75285a09b884a625706068eee0138f88545f79ec
Khang Le <ltdkhang0809@gmail.com>
2022-02-05 19:36:57 +0700

    communicate with child, parent components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
